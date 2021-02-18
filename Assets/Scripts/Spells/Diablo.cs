f8762508397767cad12962101be71728b6123821
Kirill Dudnik <prog.me.2017@gmail.com>
2021-02-18 20:17:59 +0600

    Version Beta 0.2.2! / Версия Beta 0.2.2! - added the Diablo spell; ------------------------- - добавил заклинание Diablo;

---
+++ Assets/Scripts/Spells/Diablo.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace NewOverlord
+{
+    public class Diablo : LongLastingSpell
+    {
+        private Vector3 startSpeed = Vector3.zero;
+
+        internal override void SetTarget(Transform target)
+        {
+            this.target = target;
+            SetFixedTarget(this.target.position);
+            MoveToFixedTarget(fixedTarget);
+        }
+
+    }
+}
